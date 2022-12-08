c0faea5aec6465db88d8e72ee03cd20907636c93
netmeta <moron686@gmail.com>
2022-12-08 04:49:29 +0300

    first

---
+++ t1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да  12345654321 88
+// 23432 -> да
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int check = 0;
+for (int number = n; number > 0; number /= 10)
+{
+    check = number % 10 + check * 10;
+}
+Console.Write(check);
+
+if (check == n)
+{
+    Console.Write("ДА");
+}
+else
+{
+    Console.Write("Нет");
+}
---
+++ t3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход числоN () и помещает в массив таблицу кубов чисел от 1 до N. Полученный массив вывести на экран.
+// 3 -> [1, 8, 27].
+// 5 -> [1, 8, 27, 64, 125]
+// Указание: Вывод массива вынести в отдельную функцию.
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+PrintArray(CreateQubeArray(N));
+
+
+int[] CreateQubeArray(int number)
+{
+    int[] array = new int[number];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = (i + 1) * (i + 1) * (i + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array) // Ф-я вывести массив на экран.
+{
+    Console.Write($"[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}");
+    Console.Write($"]");
+}
+
